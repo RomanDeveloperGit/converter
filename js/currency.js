461ebd01c61a8405e51a1f42c6a3c2196eb67874
roman_orlov <roman.developer@mail.ru>
2022-03-06 14:06:53 +0500

    Added API Bank

---
+++ js/currency.js
@@ -0,0 +1,32 @@
+class Currency {
+	static url = "https://www.cbr-xml-daily.ru/latest.js";
+
+	static async getObjectValues( requestTypes ) {
+		try {
+			const data = await fetch( "https://www.cbr-xml-daily.ru/latest.js" );
+			const { rates } = await data.json();
+
+			const response = { "RUB": 1 };
+
+			requestTypes.forEach( type => {
+				const currecnyInRUB = 1 / rates[type];
+				
+				if (type in rates) response[type] = parseFloat( currecnyInRUB.toFixed( 2 ) );
+				else response[type] = 0;
+			});
+
+			return response;
+		}
+		catch (error) {
+			console.error( error );
+
+			return {
+				"RUB": 1,
+				"USD": 105.81,
+				"EUR": 115.62,
+				"KZT": 0.20,
+				"INR": 1.38,
+			};
+		}
+	}
+}
--- js/index.js
+++ js/index.js
@@ -1,74 +1,71 @@
-const converter = {
-	inputs: document.querySelectorAll( ".converter__input" ),
-	selects: document.querySelectorAll( ".converter-select" ),
-	currentItemTexts: document.querySelectorAll( ".converter-select__current-text" ),
-	itemBoxes: document.querySelectorAll( ".converter-select__item-box" )
-};
+window.onload = async() => {
+	const converter = {
+		loading: document.querySelector( ".converter__loading" ),
+		inputs: document.querySelectorAll( ".converter__input" ),
+		selects: document.querySelectorAll( ".converter-select" ),
+		currentItemTexts: document.querySelectorAll( ".converter-select__current-text" ),
+		itemBoxes: document.querySelectorAll( ".converter-select__item-box" )
+	};
 
-const currencies = {
-	"RUB": 1,
-	"USD": 105.81,
-	"EUR": 115.62,
-	"BTC": 4122950,
-	"IDR": 0.007352
-};
+	const selectedCurrencies = ["RUB", "USD"]; // Выбранные валюты в 1 и 2 селектах соответственно
+	
+	const requestTypes = ["USD", "EUR", "KZT", "INR", "KRW"]; // Запрашиваемые валюты
+	const currencies = await Currency.getObjectValues( requestTypes );
 
-// Выбранные валюты в 1 и 2 селектах соответственно
-let selectedCurrencies = ["RUB", "USD"];
-
-const convertToRUB = ( typeCurrency, value ) => currencies[typeCurrency] * value;
-
-const onChangeInput = ( index, value ) => {
-	// Моя механика конвертации: введенное значение конвертирую в рубли, а из рублей в новую валюту.
-	const _index = +!index;
-
-	converter.inputs[_index].value = convertToRUB( selectedCurrencies[index], value ) / currencies[selectedCurrencies[_index]];
-};
+	converter.loading.setAttribute( "style", "display: none" );
 
 
 
-// Инициализация селектов
-converter.itemBoxes.forEach( ( element, index ) => {
-	converter.currentItemTexts[index].innerHTML = selectedCurrencies[index];
+	const convertToRUB = ( typeCurrency, value ) => currencies[typeCurrency] * value;
+	const onChangeInput = ( index, value ) => {
+		// Моя механика конвертации: введенное значение конвертирую в рубли, а из рублей в новую валюту.
 
-	for (const key in currencies) {
-		// Анатолий, объясните, пожалуйста, как это работает? Я знаю, что этой функцией мы проверяем, не перебираем ли мы случаем ключ прототипа объекта, потому что
-		// они нам не нужны. Но этот перебор и так их не перебирает.
-		// Можно сделать тут console.log( key ), мы увидим только собственные ключи данного объекта.
-		if (Object.hasOwnProperty.call( currencies, key )) {
-			let newItem = document.createElement( "li" );
-			newItem.classList.add( "converter-select__item" );
-			newItem.innerHTML = key;
+		const _index = +!index;
+		converter.inputs[_index].value = ( convertToRUB( selectedCurrencies[index], value ) / currencies[selectedCurrencies[_index]] ).toFixed( 2 );
+	};
 
-			element.appendChild( newItem );
+
+	// Инициализация селектов
+	converter.itemBoxes.forEach( ( element, index ) => {
+		converter.currentItemTexts[index].innerHTML = selectedCurrencies[index];
+
+		for (const key in currencies) {
+			// Анатолий, объясните, пожалуйста, как это работает? Я знаю, что этой функцией мы проверяем, не перебираем ли мы случаем ключ прототипа объекта, потому что
+			// они нам не нужны. Но этот перебор и так их не перебирает.
+			// Можно сделать тут console.log( key ), мы увидим только собственные ключи данного объекта.
+			if (Object.hasOwnProperty.call( currencies, key )) {
+				let newItem = document.createElement( "li" );
+				newItem.classList.add( "converter-select__item" );
+				newItem.innerHTML = key;
+
+				element.appendChild( newItem );
+			}
 		}
-	}
-});
-
-// Клик по селектам
-converter.selects.forEach( ( element, index ) => {
-	element.addEventListener( "click", event => {
-		const target = event.target;
-		const currentItemText = converter.currentItemTexts[index];
-
-		if (target.classList.value === "converter-select__item" && selectedCurrencies[index] != target.innerHTML) {
-			selectedCurrencies[index] = target.innerHTML;
-
-			currentItemText.innerHTML = target.innerHTML;
-
-			onChangeInput( index, converter.inputs[index].value );
-		}
-
-		element.classList.toggle( "converter-select_actived" );
 	});
-});
 
-// Ввод в инпуты
-converter.inputs.forEach( ( element, index ) => {
-	element.addEventListener( "input", event => {
-		const value = +event.target.value;
-		// Если слишком много ввел, то удаляем последний символ. И TOFIXED ДО 2 ЗНАКОВ
+	converter.selects.forEach( ( element, index ) => {
+		element.addEventListener( "click", event => {
+			const target = event.target;
+			const currentItemText = converter.currentItemTexts[index];
 
-		onChangeInput( index, value );
+			if (target.classList.value === "converter-select__item" && selectedCurrencies[index] != target.innerHTML) {
+				selectedCurrencies[index] = target.innerHTML;
+				currentItemText.innerHTML = target.innerHTML;
+
+				onChangeInput( index, converter.inputs[index].value );
+			}
+
+			element.classList.toggle( "converter-select_actived" );
+		});
 	});
-});
+
+	converter.inputs.forEach( ( element, index ) => {
+		element.removeAttribute( "disabled" );
+
+		element.addEventListener( "input", event => {
+			const value = +event.target.value;
+			
+			onChangeInput( index, value );
+		});
+	});
+};
